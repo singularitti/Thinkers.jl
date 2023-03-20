d43948795410f0490adaaa4d5c14e9612d2f464b
singularitti <singularitti@outlook.com>
2023-03-20 03:30:36 -0400

    Add types `Think` & `Thunk`

--- src/Thinkers.jl
+++ src/Thinkers.jl
@@ -5,4 +5,17 @@ struct ErredResult{T}
     stacktrace::Base.StackTraces.StackTrace
 end
 
+abstract type Think end
+mutable struct Thunk <: Think
+    callable
+    args::Tuple
+    kwargs::Iterators.Pairs
+    isevaluated::Bool
+    haserred::Bool
+    result::Union{Some,Nothing}
+    Thunk(callable, args::Tuple, kwargs::Iterators.Pairs) =
+        new(callable, args, kwargs, false, false, nothing)
+end
+Thunk(f, args...; kwargs...) = Thunk(f, args, kwargs)
+
 end
