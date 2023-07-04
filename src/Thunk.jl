012b73e856e8cae0942258a45343ffbee12ffd42
singularitti <singularitti@outlook.com>
2023-07-04 03:40:36 -0400

    Update docs for `reify!(thunk::Thunk)`

--- src/Thunk.jl
+++ src/Thunk.jl
@@ -85,6 +85,13 @@ Reify a `Thunk`.
 
 Calculate the value of the expression by recursively evaluating each argument and keyword of
 the `Thunk`, and then evaluating the `Thunk`'s callable with the evaluated arguments.
+
+If called again, this function will recompute everything from scratch.
+
+!!! warning
+    Some functions that the `Think` object wraps may modify their arguments or depend on
+    external state (i.e., they are not pure functions), which could lead to different
+    results upon re-evaluation.
 """
 function reify!(thunk::Thunk)
     try
