a61d0b25b9fed3c447dc22c76a19f0bd50304399
singularitti <singularitti@outlook.com>
2023-07-04 02:43:13 -0400

    Add docs to `reset!`

--- src/Thinkers.jl
+++ src/Thinkers.jl
@@ -197,6 +197,17 @@ function setargs!(think::WrappedThink, args...; kwargs...)
     return think
 end
 
+"""
+    reset!(think::Think)
+
+Reset the computation result of the `think` object.
+
+!!! warning
+    Please be aware that `reset!` does not guarantee that a `Think` object will behave
+    exactly as if it has never been evaluated. Some functions that the `Think` object
+    wraps may modify their arguments or depend on external state (i.e., they are not
+    pure functions), which could lead to different results upon re-evaluation.
+"""
 function reset!(think::Think)
     think.result = nothing
     return think
